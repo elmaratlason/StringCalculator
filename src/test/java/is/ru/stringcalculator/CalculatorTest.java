115de17a8c31dd4154f672f8157bd867c662f3aa
Elmar Atlason <elmar.atlason@gmail.com>
2017-10-10 23:46:05 +0000

    split with \n and comma

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -29,5 +29,9 @@ public class CalculatorTest {
 	public void testMultipleSplitwithOnlyNewline(){
 		assertEquals(6, Calculator.add("1\n2\n3"));
 	}
+	@Test
+	public void testMultipleSplitwithNewlineComma(){
+		assertEquals(6, Calculator.add("1\n2,3"));
+	}
 	
 }
